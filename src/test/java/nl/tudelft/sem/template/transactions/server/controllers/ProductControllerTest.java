a44dcf33fa81a82aa9e78ac8af584dcb310a04d9
Oskar Lorek <lorek@student.tudelft.nl>
2020-12-01 00:04:33 +0100

    Refactor package structure of tests to reflect package structure of src

---
+++ src/test/java/nl/tudelft/sem/template/transactions/server/controllers/ProductControllerTest.java
@@ -0,0 +1,28 @@
+package nl.tudelft.sem.template.transactions.server.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void getUserProducts() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void editHolidays() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+}
---
+++ src/test/java/nl/tudelft/sem/template/transactions/server/controllers/TransactionControllerTest.java
@@ -0,0 +1,63 @@
+package nl.tudelft.sem.template.transactions.server.controllers;
+
+import nl.tudelft.sem.template.transactions.server.entities.Product;
+import nl.tudelft.sem.template.transactions.server.entities.Transactions;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SuppressWarnings("PMD")
+class TestTransactionController {
+    private Product product;
+    private final Transactions transaction = new Transactions();
+
+
+    @BeforeEach
+    public void setup() {
+        product = new Product("bread", (float) 1.50, 10);
+        int portionsConsumed = 1;
+        String username = "Stoyan";
+        transaction.setPortions_consumed(portionsConsumed);
+        transaction.setProduct(product);
+        transaction.setTransaction_id(1);
+        transaction.setUsername(username);
+    }
+
+    @Test
+    public void constructorTest() {
+        assertNotNull(transaction);
+    }
+
+    @Test
+    public void getPortions_consumed() {
+
+        assertEquals(1, transaction.getPortions_consumed());
+    }
+
+    @Test
+    public void getProductId() {
+
+        assertEquals(product.getProductId(), transaction.getProduct_id());
+    }
+
+    @Test
+    public void getUsername() {
+
+        assertEquals("Stoyan", transaction.getUsername());
+    }
+
+    @Test
+    public void getProduct() {
+
+        assertEquals(product.getExpired(), transaction.getProduct().getExpired());
+        assertEquals(product.getPortionsLeft(), transaction.getProduct().getPortionsLeft());
+        assertEquals(product.getProductName(), transaction.getProduct().getProductName());
+        Assert.assertEquals(product.getPrice(), transaction.getProduct().getPrice(), 0.005f);
+        assertEquals(product.getTotalPortions(), transaction.getProduct().getTotalPortions());
+        assertEquals(product.getUsername(), transaction.getProduct().getUsername());
+    }
+
+}
