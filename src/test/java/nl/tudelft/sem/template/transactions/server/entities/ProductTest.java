f8e7f76f4e8fa4c88cc069557cb937a1e7392375
Oskar Lorek <lorek@student.tudelft.nl>
2020-12-01 03:29:40 +0100

    Add tests to Product entity

---
+++ src/test/java/nl/tudelft/sem/template/transactions/server/entities/ProductTest.java
@@ -0,0 +1,44 @@
+package nl.tudelft.sem.template.transactions.server.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ProductTest {
+    Product product;
+
+    @BeforeEach
+    void setup() {
+        product = new Product();
+    }
+
+    @Test
+    void getProductName() {
+        product.setProductName("Cream");
+        assertEquals("Cream", product.getProductName());
+    }
+
+    @Test
+    void getUsername() {
+        product.setUsername("Alice");
+        assertEquals("Alice", product.getUsername());
+    }
+
+    @Test
+    void getPrice() {
+        product.setPrice(128.0f);
+        assertEquals(128.0f, product.getPrice());
+    }
+
+    @Test
+    void getTotalPortions() {
+        product.setTotalPortions(12);
+        assertEquals(12, product.getTotalPortions());
+    }
+
+    @Test
+    void getExpired() {
+        assertEquals(0, product.getExpired());
+    }
+}
