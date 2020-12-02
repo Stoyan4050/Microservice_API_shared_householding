1770aedb372852cde0f459fd3e4220b52ac208eb
Oskar Lorek <lorek@student.tudelft.nl>
2020-12-02 21:52:09 +0100

    Change the expiration time of JWT from 1 day to 3 days

--- transactions/src/main/java/nl/tudelft/sem/transactions/config/JwtConf.java
+++ transactions/src/main/java/nl/tudelft/sem/transactions/config/JwtConf.java
@@ -13,7 +13,7 @@ public class JwtConf {
     @Value("${security.jwt.prefix:Bearer }")
     private String prefix;
 
-    @Value("${security.jwt.expiration:#{24*60*60}}")
+    @Value("${security.jwt.expiration:#{3*24*60*60}}")
     private int expiration;
 
     @Value("${security.jwt.secret:OZ9y3ADENrII}")
