20e8e708631e36f4e8568e63b40054a4ca84b0d3
Atanas Pashov <A.I.Pashov@student.tudelft.nl>
2020-12-02 20:52:33 +0200

    Connected the gateway microservice to the transactions and requests microservices.
    Also additionally get username for /allProducts of the user making the request in the product controller (still testing it).

--- requests/src/main/java/nl/tudelft/sem/requests/Application.java
+++ requests/src/main/java/nl/tudelft/sem/requests/Application.java
@@ -3,11 +3,13 @@ package nl.tudelft.sem.requests;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableJpaRepositories("nl.tudelft.sem.requests.repositories")
-@EntityScan("nl.tudelft.sem.template")
+@EntityScan("nl.tudelft.sem.requests.entities")
 public class Application {
 
     public static void main(String[] args) {
--- transactions/src/main/java/nl/tudelft/sem/transactions/Application.java
+++ transactions/src/main/java/nl/tudelft/sem/transactions/Application.java
@@ -1,17 +1,26 @@
 package nl.tudelft.sem.transactions;
 
+import nl.tudelft.sem.transactions.config.JwtConf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableJpaRepositories("nl.tudelft.sem.transactions.repositories")
-@EntityScan("nl.tudelft.sem.template")
+@EntityScan("nl.tudelft.sem.transactions.entities")
 
 @SuppressWarnings("PMD")
 public class Application {
     public static void main(String[] args) {
 	SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public JwtConf jwtConfig() {
+        return new JwtConf();
     }
 }
