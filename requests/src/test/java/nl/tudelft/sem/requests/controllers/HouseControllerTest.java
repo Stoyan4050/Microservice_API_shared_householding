69f23d63ef997272d90443817c76e4abd83f4c50
Ina Lupu <G.Lupu@student.tudelft.nl>
2020-12-17 23:48:57 +0100

    added tests for userJoiningHouse() and userLeavingHouse() methods of the House Controller

--- requests/src/test/java/nl/tudelft/sem/requests/controllers/HouseControllerTest.java
+++ requests/src/test/java/nl/tudelft/sem/requests/controllers/HouseControllerTest.java
@@ -20,6 +20,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 
 /**
@@ -100,4 +102,167 @@ public class HouseControllerTest {
         expected.add(user);
         assertEquals(expected, result);
     }
+
+    @Test
+    public void testUserJoiningHouse() {
+        Optional<House> house = Optional.of(new House(1, "CoolHouse"));
+        final User user1 = new User("Malwina");
+        final User user2 = new User("Malwina");
+        user1.setHouse(house.get());
+        user2.setHouse(null);
+
+        Set<User> users = new HashSet<>();
+        users.add(user1);
+        house.get().setUsers(users);
+
+        Set<User> updatedUsers = new HashSet<>();
+        updatedUsers.add(user1);
+        updatedUsers.add(user2);
+
+        when(houseRepository.findById(1)).thenReturn(house);
+        when(userRepository.findById("Malwina")).thenReturn(Optional.of(user1));
+        when(userRepository.findById("Mocha")).thenReturn(Optional.of(user2));
+
+        houseController.userJoiningHouse("Mocha", 1);
+
+        assertEquals(user2.getHouse().getHouseNr(), 1);
+        //assertEquals(updatedUsers, user2.getHouse().getUsers());
+        //TODO -> the users should also get updated -> should return true but it returns false
+    }
+
+    @Test
+    public void testUserLeavingHouse() {
+        final Optional<House> house = Optional.of(new House(1, "CoolHouse"));
+        final User user1 = new User("Malwina");
+        final User user2 = new User("Mocha");
+        user1.setHouse(house.get());
+        user2.setHouse(house.get());
+
+        Set<User> users = new HashSet<>();
+        users.add(user1);
+        users.add(user2);
+        house.get().setUsers(users);
+
+        when(houseRepository.findById(1)).thenReturn(house);
+        when(userRepository.findById("Malwina")).thenReturn(Optional.of(user1));
+        when(userRepository.findById("Mocha")).thenReturn(Optional.of(user2));
+
+        final ResponseEntity<House> result = houseController.userLeavingHouse("Mocha", 1);
+
+        final ResponseEntity<House> expected = new ResponseEntity("You successfully removed "
+            + "Mocha from house number 1!", HttpStatus.OK);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testUserLeavingHouseAndDeleteHouse() {
+        final Optional<House> house = Optional.of(new House(1, "CoolHouse"));
+        final User user = new User("Mocha");
+        user.setHouse(house.get());
+
+        Set<User> users = new HashSet<>();
+        users.add(user);
+        house.get().setUsers(users);
+
+        when(houseRepository.findById(1)).thenReturn(house);
+        when(userRepository.findById("Mocha")).thenReturn(Optional.of(user));
+
+        final ResponseEntity<House> result = houseController.userLeavingHouse("Mocha", 1);
+
+        final ResponseEntity<House> expected = new ResponseEntity("You successfully removed "
+            + "Mocha from house number 1!", HttpStatus.OK);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testUserLeavingHouseDifferentHousehold() {
+        final Optional<House> house1 = Optional.of(new House(1, "CoolHouse"));
+        final Optional<House> house2 = Optional.of(new House(2, "NotSoCoolHouse"));
+
+        //users for 1st house
+        final User user1 = new User("Malwina");
+        final User user2 = new User("Mocha");
+        user1.setHouse(house1.get());
+        user2.setHouse(house1.get());
+
+        //users for 2nd house
+        final User user3 = new User("Hungry");
+        user3.setHouse(house2.get());
+
+        //setting the users for 1st house
+        Set<User> users1 = new HashSet<>();
+        users1.add(user1);
+        users1.add(user2);
+        house1.get().setUsers(users1);
+
+        //setting the users for 2st house
+        Set<User> users2 = new HashSet<>();
+        users2.add(user3);
+        house2.get().setUsers(users2);
+
+        when(houseRepository.findById(1)).thenReturn(house1);
+        when(houseRepository.findById(2)).thenReturn(house2);
+        when(userRepository.findById("Malwina")).thenReturn(Optional.of(user1));
+        when(userRepository.findById("Mocha")).thenReturn(Optional.of(user2));
+        when(userRepository.findById("Hungry")).thenReturn(Optional.of(user3));
+
+        final ResponseEntity<House> result = houseController.userLeavingHouse("Hungry", 1);
+
+        final ResponseEntity<House> expected = new ResponseEntity("You can not remove a user"
+            + " from a different household!", HttpStatus.FORBIDDEN);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testUserLeavingHouseUserWithNoHouse() {
+        final Optional<House> house = Optional.of(new House(1, "CoolHouse"));
+        final User user1 = new User("Malwina");
+        final User user2 = new User("Mocha");
+        user1.setHouse(house.get());
+        user2.setHouse(null);
+
+        Set<User> users = new HashSet<>();
+        users.add(user1);
+        house.get().setUsers(users);
+
+        when(houseRepository.findById(1)).thenReturn(house);
+        when(userRepository.findById("Malwina")).thenReturn(Optional.of(user1));
+        when(userRepository.findById("Mocha")).thenReturn(Optional.of(user2));
+
+        final ResponseEntity<House> result = houseController.userLeavingHouse("Mocha", 1);
+
+        final ResponseEntity<House> expected = new ResponseEntity("The user does not "
+            + "have a house!", HttpStatus.FORBIDDEN);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testUserLeavingHouseNotFound() {
+        final Optional<House> house = Optional.of(new House(1, "CoolHouse"));
+        final User user1 = new User("Malwina");
+        final User user2 = new User("Mocha");
+        user1.setHouse(house.get());
+        user2.setHouse(house.get());
+
+        Set<User> users = new HashSet<>();
+        users.add(user1);
+        users.add(user2);
+        house.get().setUsers(users);
+
+        when(houseRepository.findById(1)).thenReturn(house);
+        when(userRepository.findById("Malwina")).thenReturn(Optional.of(user1));
+        when(userRepository.findById("Mocha")).thenReturn(Optional.of(user2));
+
+        final ResponseEntity<House> result = houseController.userLeavingHouse("Mocha", 2);
+
+        final ResponseEntity<House> expected = new ResponseEntity("The user or the house "
+            + "were not found, please check again!", HttpStatus.NOT_FOUND);
+
+        assertEquals(expected, result);
+    }
+
 }
